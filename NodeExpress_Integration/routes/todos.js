5858a78cadcadfade8897e184f35f06e6afc6bf8
Cokaine2912 <niraj28352@gmail.com>
2024-03-09 13:02:10 +0530

    Expense Task 24 Complete

---
+++ NodeExpress_Integration/routes/todos.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+let todos = [];
+router.get("/", (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    return res.status(201).json(newTodo);
+});
+router.post("/deleteTodo/:id", (req, res, next) => {
+    const todelId = req.params.id;
+    console.log(todelId);
+    const todelObjectIndex = todos.findIndex(obj => obj.id === todelId);
+    if (todelObjectIndex >= 0 && todelObjectIndex < todos.length) {
+        todos.splice(todelObjectIndex, 1);
+    }
+    else {
+        return res.status(404).json({ msg: "Entry Not Found" });
+    }
+    return res.status(200).json(todos);
+});
+router.post("/edit/:id", (req, res, next) => {
+    const toeditId = req.params.id;
+    const new_obj = req.body;
+    const toeditObjectIndex = todos.findIndex(obj => obj.id === toeditId);
+    if (toeditObjectIndex >= 0 && toeditObjectIndex < todos.length) {
+        todos[toeditObjectIndex] = new_obj;
+    }
+    else {
+        return res.status(404).json({ msg: "Entry Not Found" });
+    }
+    return res.status(201).json({ todos });
+});
+exports.default = router;
---
+++ NodeExpress_Integration/routes/todos.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+
+import {Todo} from "../models/todos"
+import { todo } from "node:test";
+
+const router = Router()
+
+let todos: Todo[] = []
+
+router.get("/",(req,res,next)=>{
+    res.status(200).json({todos : todos})
+})
+
+router.post("/todo",(req,res,next)=>{
+    const newTodo : Todo = {
+        id : new Date().toISOString(),
+        text : req.body.text
+    }
+
+    todos.push(newTodo)
+    return res.status(201).json(newTodo)
+})
+
+router.post("/deleteTodo/:id",(req,res,next)=>{
+    const todelId = req.params.id
+
+    console.log(todelId)
+
+    const todelObjectIndex = todos.findIndex(obj => obj.id === todelId);
+
+    if (todelObjectIndex >= 0 && todelObjectIndex < todos.length) {
+        todos.splice(todelObjectIndex, 1);
+    }
+    else {
+        return res.status(404).json({msg : "Entry Not Found"})
+    }
+
+    return res.status(200).json(todos)
+
+})
+router.post("/edit/:id" , (req,res,next) => {
+    const toeditId = req.params.id
+    const new_obj = req.body
+    const toeditObjectIndex = todos.findIndex(obj => obj.id === toeditId);
+
+    if (toeditObjectIndex >= 0 && toeditObjectIndex < todos.length) {
+        todos[toeditObjectIndex] = new_obj;
+    }
+    else {
+        return res.status(404).json({msg : "Entry Not Found"})
+    }
+    return res.status(201).json({todos})
+
+})
+
+export default router
